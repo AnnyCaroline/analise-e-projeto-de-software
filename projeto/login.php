2d32c293a1b3fb0460911dc69c3a468c022c16ed
Anny Chagas <annycarolinegnr@gmail.com>
2022-06-02 00:04:03 -0300

    Fix db name

--- projeto/login.php
+++ projeto/login.php
@@ -2,7 +2,7 @@
     define('HOST', '127.0.0.1');
     define('USER', 'root');
     define('PASSWORD', '');
-    define('DB', 'my-login');
+    define('DB', 'projeto');
     
     $conexao = mysqli_connect(HOST, USER, PASSWORD, DB) or die ('Não foi possível conectar');
 
